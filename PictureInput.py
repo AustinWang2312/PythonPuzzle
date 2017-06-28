5293e7a98017bcecd8598ea126bd0785fac39169
AustinWang2312 <austinx.wang@intel.com>
2017-06-28 15:45:49 -0700

    Final Draft

--- PictureInput.py
+++ PictureInput.py
@@ -1,10 +1,10 @@
 import re
-filename= input("Text filename:");
-#input_file_name = 'input_file.txt'
-#output_file_name = 'austin_output_file.txt'
-#input_file_handle = open(filename, 'r')
-#output_file_handle = open(filename, 'w')
-with open(filename)as f:
+input_file_name = 'input_file.txt'
+output_file_name = 'my_output_file.txt'
+input_file_handle = open(input_file_name, 'r')
+output_file_handle = open(output_file_name, 'w')
+#filename= input("Text filename:");
+with open(input_file_name)as f:
 	contents=f.read();
 	edit1= contents.replace("async",""); 
 	edit1= edit1.replace('href',"")
@@ -23,21 +23,23 @@ with open(filename)as f:
 	edit1= [s[4:-1] for s in edit1]
 	edit1= edit1[:69]
 	count=0
-	def print_output(s,c):
-		print("<p>" + str(c) )
-		c=c+1	
-		print("<>br><img",s) 
-		print("")
+	def print_output(s):
+		output_file_handle.write ("<p>" + str(edit1.index(s)+1)+".")
+		output_file_handle.write ("\n")
+		output_file_handle.write ("<>br><img "+s) 
+		output_file_handle.write ("\n")
+		output_file_handle.write ("\n")
+
 	for s in edit1:
-		print_output(s,count+1);
-		count = count + 1
+		print_output(s);
+	#	count = count + 1
 #	edit2= edit1.replace('"data-flickr-embed="true"',"") 
 #	edit3= {x.replace("async","") for x in edit2}
 #	edit4= {x.replace('charset="utf-8"></script>',"") for x in edit3}
 #	edit5= {x.replace('<a',"") for x in edit4}
 #	edit6= {x.replace('src="//embedr.flickr.com/assets/client-code.js"',"") for x in edit5}
 	#edit2=[];
-#adf
+
 	#for i in edit1:
 		#a= re.search('src=(.+?).jpg', edit1);
 		#if a:
@@ -45,3 +47,7 @@ with open(filename)as f:
 
 
 
+#input_file_name = 'input_file.txt'
+#output_file_name = 'austin_output_file.txt'
+#input_file_handle = open(filename, 'r')
+#output_file_handle = open(filename, 'w')
