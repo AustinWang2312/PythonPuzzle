40032413d56053782516c8a5a0bfd7bee9d77eb8
AustinWang2312 <austinx.wang@intel.com>
2017-07-18 14:46:16 -0700

    optimized part of code

--- server_reserve/frontend/dynamic.py
+++ server_reserve/frontend/dynamic.py
@@ -15,19 +15,26 @@ with open("/var/www/html/index.html","w") as r:
 	print("</h3>", file = r)
 	print("<body>", file = r)
 	with open("./cpuinfo.json", "r") as f:
+		brlist = ["{","}",",","\""]
 		jsondata = f.read()	
-		jsondata = jsondata.replace("{", "<br>" )
-		jsondata = jsondata.replace("}", "<br>" )
-		jsondata = jsondata.replace(",", "<br>" )
-		jsondata = jsondata.replace("\"", "<br>" )		
-		jsondata = jsondata.replace("<br>: <br>", "" )
+		
+		for i in brlist:
+			jsondata = jsondata.replace(i, "<br>" )
+	#	jsondata = jsondata.replace("}", "<br>" )
+	#	jsondata = jsondata.replace(",", "<br>" )
+	#	jsondata = jsondata.replace("\"", "<br>" )		
+		jsondata = jsondata.replace("<br>: <br>", "<br>" )
+		
+		title = ["Memory Info:","Network Interface:","Distribution:","CPU Architecture:","Kernel:","Users:"]
+		for j in title:
+			jsondata = jsondata.replace(j, "<font style=\"background-color:ff8e3a\">"+j+"</font>")
 		jsondata = jsondata.replace("Host:", "<font style=\"background-color: #EF4323\">Host:</font> ")
-		jsondata = jsondata.replace("Memory Info:", "<font style=\"background-color: #ff8e3a \">Memory Info:</font> ")
-		jsondata = jsondata.replace("Network Interface:", "<font style=\"background-color: #ff8e3a\">Network Interface:</font> ")
-		jsondata = jsondata.replace("Distribution:", "<font style=\"background-color: #ff8e3a\">Distribution:</font> ")
-		jsondata = jsondata.replace("CPU Architecture:", "<font style=\"background-color: #ff8e3a\">CPU Architecture:</font> ")
-		jsondata = jsondata.replace("Kernel:", "<font style=\"background-color: #ff8e3a\">Kernel:</font> ")
-		jsondata = jsondata.replace("Users:", "<font style=\"background-color: #ff8e3a\">Users:</font> ")
+	#	jsondata = jsondata.replace("Memory Info:", "<font style=\"background-color: #ff8e3a \">Memory Info:</font> ")
+	#	jsondata = jsondata.replace("Network Interface:", "<font style=\"background-color: #ff8e3a\">Network Interface:</font> ")
+	#	jsondata = jsondata.replace("Distribution:", "<font style=\"background-color: #ff8e3a\">Distribution:</font> ")
+	#	jsondata = jsondata.replace("CPU Architecture:", "<font style=\"background-color: #ff8e3a\">CPU Architecture:</font> ")
+	#	jsondata = jsondata.replace("Kernel:", "<font style=\"background-color: #ff8e3a\">Kernel:</font> ")
+	#	jsondata = jsondata.replace("Users:", "<font style=\"background-color: #ff8e3a\">Users:</font> ")
 
 		print(jsondata, file = r)
 		print("</body>", file = r)
